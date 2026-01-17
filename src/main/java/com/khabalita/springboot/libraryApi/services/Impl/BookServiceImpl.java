9e3530771badc4e778631e3388358cd7e0345420
khabalita <nicolazaro91@gmail.com>
2026-01-17 10:49:18 +0100

    updated bookServiceImpl whitout mapper inyection

--- src/main/java/com/khabalita/springboot/libraryApi/services/Impl/BookServiceImpl.java
+++ src/main/java/com/khabalita/springboot/libraryApi/services/Impl/BookServiceImpl.java
@@ -2,13 +2,9 @@ package com.khabalita.springboot.libraryApi.services.Impl;
 
 import com.khabalita.springboot.libraryApi.dto.BookDto;
 import com.khabalita.springboot.libraryApi.entities.Book;
-import com.khabalita.springboot.libraryApi.mapper.AuthorMapper;
 import com.khabalita.springboot.libraryApi.mapper.BookMapper;
-import com.khabalita.springboot.libraryApi.mapper.CategoryMapper;
-import com.khabalita.springboot.libraryApi.mapper.EditorialMapper;
 import com.khabalita.springboot.libraryApi.repository.*;
 import com.khabalita.springboot.libraryApi.services.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,30 +14,22 @@ public class BookServiceImpl
 
     private final BookRepository bookRepository;
     private final BookMapper bookMapper;
-
-    @Autowired
     private AuthorRepository authorRepository;
-
-    @Autowired
     private EditorialRepository editorialRepository;
-
-    @Autowired
     private CategoryRepository categoryRepository;
 
-    @Autowired
-    private AuthorMapper authorMapper;
-
-    @Autowired
-    private EditorialMapper editorialMapper;
-
-    @Autowired
-    private CategoryMapper categoryMapper;
 
     public BookServiceImpl(BookRepository bookRepository,
-                           BookMapper bookMapper){
+                           BookMapper bookMapper,
+                           AuthorRepository authorRepository,
+                           EditorialRepository editorialRepository,
+                           CategoryRepository categoryRepository){
         super(bookRepository, bookMapper);
         this.bookRepository = bookRepository;
         this.bookMapper = bookMapper;
+        this.authorRepository = authorRepository;
+        this.categoryRepository = categoryRepository;
+        this.editorialRepository = editorialRepository;
     }
 
 }
